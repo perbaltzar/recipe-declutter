99fefc247841342a293c74cecf8963520b9ca842
perbaltzar <per.baltzar@hiberworld.com>
2025-09-04 12:50:04 +0200

    make scraper server work

--- apps/scraper/src/index.ts
+++ apps/scraper/src/index.ts
@@ -1,11 +1,19 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import { getScrapedRecipe } from "./scraper";
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello Hono!"));
+app.post("/scrape", async (c) => {
+  const { url } = await c.req.json();
+  const result = await getScrapedRecipe(url);
+  return c.json(result);
+});
 
 serve({
   fetch: app.fetch,
   port: 4000, // <— this is the port
 });
+
+console.log("Server is running on http://localhost:4000");
